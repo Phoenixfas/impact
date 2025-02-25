570b575f7dff9cd9d885fabaa7123fe4b776d4e2
Fasika Dagnachew <fasika1515@gmail.com>
2025-02-25 04:20:00 +0300

    hero letter spacing

--- app/HeroContent.tsx
+++ app/HeroContent.tsx
@@ -13,7 +13,7 @@ export default function HeroContent() {
 
   return (
     <div className='hero-con relative w-full h-screen flex flex-col items-center sm:items-start justify-center px-5 sm:px-10 md:px-20 pt-20 sm:pb-20 overflow-hidden'>
-        <h1 ref={heroH} className='hero-h relative max-w-[800px] text-[1.8rem] leading-[2.5rem] sm:text-5xl md:text-[4rem] md:leading-[5rem] lg:text-[5rem] lg:leading-[6rem] tracking-widest text-center sm:text-left font-black mb-10 text-[var(--background)] uppercase drop-shadow-[0_0_5px_var(--background)] sm:font-outline-black  space-mono-bold'>Crafting Experiences <br /> Creating Impact</h1>
+        <h1 ref={heroH} className='hero-h relative max-w-[800px] text-[1.8rem] leading-[2.5rem] sm:text-5xl md:text-[4rem] md:leading-[5rem] lg:text-[5rem] lg:leading-[6rem] tracking-[1rem] text-center sm:text-left font-black mb-10 text-[var(--background)] uppercase drop-shadow-[0_0_5px_var(--background)] sm:font-outline-black  space-mono-bold'>Crafting Experiences <br /> Creating Impact</h1>
         {/* <p className='text-[var(--foreground)] text-sm sm:text-lg text-center'>Millennium Hall, June 19-21, 2025</p> */}
     </div>
   )
