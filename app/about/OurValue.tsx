00114d189a0fba0799b74d83fc086e15747f4ddd
Fasika Dagnachew <fasika1515@gmail.com>
2025-02-27 09:08:56 +0300

    about page overflow fixed

--- app/about/OurValue.tsx
+++ app/about/OurValue.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OurValue() {
   return (
     <div className='relative w-full my-28 py-28 px-8 sm:px-[100px] bg-[var(--foreground)] flex flex-col items-center text-center text-[var(--background)]'>
-        <div className="absolute w-[1000px] h-[1000px] bg-[radial-gradient(#ffffff33_0%,_transparent_50%)]"></div>
+        <div className="absolute w-full h-full max-w-[1000px] max-h-[1000px] bg-[radial-gradient(#ffffff33_0%,_transparent_50%)]"></div>
         <h2 className='text-4xl sm:text-6xl mb-10 black-ops'>What Drives Us</h2>
         <p className='text-2xl text-center mb-5'><span className='font-bold'>Creativity</span> – Bringing fresh and innovative ideas to every event.</p>
         <p className='text-2xl text-center mb-5'><span className='font-bold'>Precision</span> – Meticulous planning to ensure seamless execution.</p>
